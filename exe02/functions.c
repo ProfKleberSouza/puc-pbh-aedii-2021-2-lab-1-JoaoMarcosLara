16e660fa8a38cce2f26dac4f24a63b90483f8bea
João  Marcos Lara Paula Rodrigues <89215665+JoaoMarcosLara@users.noreply.github.com>
2021-08-20 22:53:57 +0000

    exercicio1 e exercicio2

--- exe02/functions.c
+++ exe02/functions.c
@@ -1,7 +1,11 @@
 
 #include "functions.h"
 
-int somar(int n1, int n2){
-
-    return n1+n2;
+int menor_num(int vet[], int n){
+int menor = vet[0];
+for(int i = 0;i<n;i++){
+  if(menor > vet[i])
+  menor = vet[i];
+}
+return menor;
 }
--- exe02/main.c
+++ exe02/main.c
@@ -1,18 +1,18 @@
 #include <stdio.h>
 #include "functions.h"
+#define MAX 10
 
 int main() {
-
-    int a, b, s;
-  
-  // ENTRADA DE DADOS
-  scanf("%d %d", &a, &b);
-
-  // PROCESSAMENTO - EXECUTANDO A FUNCAO
-  s = somar(a, b);
-
-  // SAIDA - IMPRIMINDO O RESULTADO
-  printf("SOMA = %d\n", s);
-
+int t=0;
+int vet[MAX];
+int menor;
+printf("digite o tamanho do vetor <=10\n");
+scanf("%d",&t);
+printf("digite um numero para preencher o vetor\n");
+for(int i=0;i<t;i++){
+  printf("digite um numero vetor na posicao[%d]\n",i);
+  scanf("%d",&vet[i]);
+}
+menor = menor_num(vet[],t);
   return(0);
 }
